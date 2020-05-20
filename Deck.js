1f6d5b86b0d3b2aa996275a7cc3d694c1391ed32
Alireza B <Alireza@simor.org>
2020-05-20 12:36:59 -0700

    moved styles of deck to DeckStyles

--- Deck.js
+++ Deck.js
@@ -9,6 +9,7 @@ import {deleteDeck} from './actions/index';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Select from 'react-select';
 import {Card, Button} from 'react-native-paper'
+import {styles} from './styles/DeckStyles'
 
 
 class Deck extends Component {
@@ -19,13 +20,21 @@ class Deck extends Component {
     });
 
   deleteThisDeck() {
-     const name = this.props.name;
-     //const {deckId}= this.props;
-     const deckId = this.props.deckId;
-     this.props.deleteDeck(deckId);
-     this.props.navigation.navigate('Home')
+    <TextInput
+     style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+     onChangeText={text => onChangeText(text)}
+     placeholder={"Enter yes or Yes to delete deck"}
 
-   }
+   />
+
+        const deckId = this.props.deckId;
+        //const {deckId}= this.props;
+        this.props.deleteDeck(deckId);
+        this.props.navigation.navigate('Home')
+
+    }
+
+
 
      AddCard = ()=> {
      this.props.navigation.navigate(
@@ -116,103 +125,3 @@ const mapStateToProps = (state, { route }) => ({
 
 
 export default connect(mapStateToProps,{deleteDeck})(Deck)
-
-const styles = StyleSheet.create({
-   container: {
-      paddingTop: 5,
-      height: 700,
-      backgroundColor: '#ff7f50',
-   },
-   card: {
-     padding: 10,
-     margin: 10,
-     backgroundColor:'#faebd7',
-     borderRadius: 20,
-   },
-
-   card10: {
-     margin: 10,
-     backgroundColor: '#f0f8ff',
-     borderRadius: 5,
-     padding: 20,
-     shadowColor: 'rgba(0,0,0,0.5)',
-     shadowOffset: {
-       width: 1,
-       height: 4,
-     },
-     shadowOpacity: 0.5,
-   },
-
-   deckName : {
-     margin: 5,
-     marginLeft: 30,
-     height: 40,
-     fontSize: 20,
-     marginTop: 20,
-   },
-
-   text : {
-     margin: 10,
-     marginLeft: 20,
-     height: 40,
-     fontSize: 15,
-     marginTop: 10,
-     opacity: .5,
-     borderRadius:20,
-
-   },
-   iftexts : {
-     margin: 10,
-     marginLeft: 15,
-     marginBottom:50,
-     height: 50,
-     fontSize: 30,
-     marginTop: 30,
-     borderRadius:20,
-
-   },
-
-   deleteButton:{
-     marginTop:100,
-   },
-
-   submitButton: {
-
-      backgroundColor: '#ffa07a',
-      padding: 15,
-      margin: 15,
-      marginBottom: 10 ,
-      height: 50,
-      borderRadius:5,
-   },
-
-   submitButton2: {
-
-      backgroundColor: '#20b2aa',
-      padding: 15,
-      margin: 15,
-      height: 50,
-      marginBottom: 50 ,
-      borderRadius:5,
-      fontSize:10,
-      marginBottom:100,
-   },
-   submitButtonText:{
-      color: 'white',
-      marginLeft: 95,
-      justifyContent : "center",
-   },
-   submitButtonText2:{
-      color: 'black',
-      marginLeft: 95,
-      justifyContent : "center",
-
-   },
-   tt:{
-      color: 'black',
-      marginLeft: 30,
-      marginBottom: 80,
-      marginTop:10,
-      justifyContent : "center",
-   }
-});
